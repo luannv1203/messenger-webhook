90cfa350dadebd2cf4aa3dd943ea4df0f5401d52
Luan Nguyen Van <luannv@timebird.org>
2021-10-28 14:07:25 +0700

    Update

--- controllers/ChatBotController.js
+++ controllers/ChatBotController.js
@@ -37,7 +37,6 @@ module.exports = {
 
         // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
         console.log('Sender PSID: ' + sender_psid);
