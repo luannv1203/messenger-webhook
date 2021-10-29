7194b6b7005ccf2ba483b0506ac6e58952632a67
Luan Nguyen Van <luannv@timebird.org>
2021-10-29 10:06:24 +0700

    Update

--- controllers/ChatBotController.js
+++ controllers/ChatBotController.js
@@ -68,8 +68,32 @@ function handleMessage(sender_psid, received_message) {
   // Check if the message contains text
   if (received_message.text) {    
     // Create the payload for a basic text message
+    // response = {
+    //   "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    // }
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Ban muon hoi toi cai gi?",
+            // "subtitle": "Tap a button to answer.",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
     }
   } else if(received_message.attachments) {
     // Gets the URL of the message attachment
