efb41e2fc21f8207a6263595521bf51ab5fd77cb
Luan Nguyen Van <luannv@timebird.org>
2021-10-29 10:21:44 +0700

    Fix

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 var express = require("express");
-var bodyPaser = require("body-parser");
-const { getWebHook, postWebHook } = require("./controllers/ChatBotController");
+var bodyPaser = require("body-parser")
+
+const { postWebHook, getWebHook } = require("./controllers/ChatBotController");
+
+require('dotenv').config()
 
 if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
-  require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 } else {
-  require("dotenv").config();
+ require("dotenv").config();
 }
 var app = express();
 
 app.use(bodyPaser.json());
-app.use(bodyPaser.urlencoded({ extended: true }))
 // app.get("/", function (req, res) {
-//   res.send("Hello World!");
+// res.send("Hello World!");
 // });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
-  console.log('ahihihii');
 })
+
+app.get("/", function(request, response) {
+ response.render("index");
+});
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.get("/", function(request, response)  {
-  response.render("index");
-});
+app.post('/webhook', postWebHook);
 
-app.get('/webhook', getWebHook)
-app.post('/webhook', postWebHook)
-
+// Adds support for GET requests to our webhook
+app.get('/webhook', getWebHook);
